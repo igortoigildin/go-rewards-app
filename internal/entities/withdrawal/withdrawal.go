1abaeb7d48767462f2c1f7a8f093eed4860d5aff
Igor Toigildin <toygildinigor@gmail.com>
2024-08-11 22:42:19 +0300

    static check update

--- internal/entities/withdrawal/withdrawal.go
+++ internal/entities/withdrawal/withdrawal.go
@@ -3,7 +3,7 @@ package withdrawal
 import "time"
 
 type Withdrawal struct {
-	Id     string    `json:"id"`
+	ID     string    `json:"id"`
 	Sum    int       `json:"sum"`
 	Date   time.Time `json:"date"`
 	UserID int64     `json:"user_id"`
